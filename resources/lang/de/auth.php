ba1a301401b304c4f14d698125a4bf2204f168f8
Christoph Dunkake <christoph@dragoneyes.org>
2021-02-09 22:31:04 +0100

    env setup

---
+++ resources/lang/de/auth.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+
+    'failed' => 'Diese Kombination ist den System leider nicht bekannt.',
+    'password' => 'Das eingegebene Passwort ist nicht korrekt.',
+    'throttle' => 'Nun mal langsam. Du kannst es in :seconds Sekunde nochmals versuchen.',
+
+];
