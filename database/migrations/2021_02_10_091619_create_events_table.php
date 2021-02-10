4856cd2d7c6867f8d19a99e241b1ace479877ca7
Christoph Dunkake <christoph@dragoneyes.org>
2021-02-10 09:32:01 +0100

    events

---
+++ database/migrations/2021_02_10_091619_create_events_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->boolean('isOwnEvent')->default(true);
+            $table->boolean('isOnline')->default(true);
+            $table->boolean('isOnsite')->default(true);
+            $table->timestamps();
+            $table->bigInteger('created_by')->unsigned()->nullable();
+            $table->bigInteger('updated_by')->unsigned()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
