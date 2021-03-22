3f2c6e710dfafe10d913178813479029de069e00
Christoph Dunkake <christoph@dragoneyes.org>
2021-03-22 22:40:43 +0100

    readme for UGMv2

--- resources/views/layouts/legacyapp.blade.php
+++ resources/views/layouts/legacyapp.blade.php
@@ -118,7 +118,7 @@ $log = \App\Http\Controllers\LogLegacyController::write('pageview', $page);
         <div class="container mt-4">
             <p>FM Zürich ist <strong>kein</strong> offizieller Kommunikationskanal von Claris Inc., sondern eine nicht kommerzielle Initiative von FileMaker-Benutzern.</p>
             <p>fmzürich.ch basiert auf <a href="https://github.com/Dragon-Eyes/user-group-manager-v2" target="_blank">User-Group-Manager-v2</a> entwickelt von Dragon Eyes und lizensiert unter MIT License<br>
-                {mit Liebe, PHP {{ PHP_VERSION }}, Laravel {{ Illuminate\Foundation\Application::VERSION }}, Jetstream, Livewire und Tailwind}.</p>
+                {mit Liebe, PHP {{ PHP_VERSION }}, Laravel {{ Illuminate\Foundation\Application::VERSION }} und Jetstream - bald auch die öffentlichen Seiten zudem mit Livewire und Tailwind}.</p>
         </div>
     </div>
 </div> <!-- end pagecontainer -->
