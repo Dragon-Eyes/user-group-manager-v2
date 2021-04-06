dbf41f94c3304b3d02793e757f103aca8fdcd645
Christoph Dunkake <christoph@dragoneyes.org>
2021-04-06 12:49:01 +0200

    #177618002

--- app/Http/Controllers/RegistrationLegacyController.php
+++ app/Http/Controllers/RegistrationLegacyController.php
@@ -2,37 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use App\Models\RegistrationLegacy;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class RegistrationLegacyController extends Controller
 {
-    /**
-     * @return \Illuminate\Http\Response
-     */
-    /*    public static function index() {
-            $eventIds = [
-                'Stammtisch 2021-03',
-                'Stammtisch 2021-04',
-                'Stammtisch 2021-05',
-                'Stammtisch 2021-06',
-                'Stammtisch 2021-08',
-            ];
-            $registrations = [];
-            foreach($eventIds as $eventId) {
-                $registrations[$eventId] = self::get_by_event_title($eventId);
-            }
-        $eventsFuture = DB::select('SELECT * FROM events WHERE date > (SELECT DATE_ADD(CURRENT_DATE, INTERVAL -1 DAY ))');
-        $registrations = [];
-        foreach($eventsFuture as $event) {
-            if($event->isOwnEvent) {
-                $registrations[$event->id] = self::get_by_event_id($event->id);
-            }
-        }
-        return view('legacy.indexcomponents', compact('registrations', 'eventsFuture'));
-    }*/
-
     /**
      * Store a new event registration in database and reload page.
      *
@@ -42,6 +18,11 @@ class RegistrationLegacyController extends Controller
         $requestValidated = $request->validate([
         ]);
         $registrationData = $request->post()['registration'];
+        $event = Event::find((int)$registrationData['event_id']);
+        if(!$event || !$event->is_registration_open) {
+            header("Location: /");
+            exit();
+        }
         $registrationType = $registrationData['presence'] == 'onsite' ? 0 : 1;
         $now = new \DateTime();
         $registrationCreated = $now->format('Y-m-d H:i:s');
@@ -50,25 +31,6 @@ class RegistrationLegacyController extends Controller
         header("Location: /");
         exit();
     }
-
-/*    public static function pastevents() {
-        $eventIds = [
-            'Stammtisch 2021-02',
-            'Stammtisch 2021-01',
-            'Stammtisch 2020-11',
-            'Stammtisch 2020-08',
-            'Stammtisch 2020-05',
-            'Stammtisch 2020-04',
-            'Stammtisch 2020-02',
-            'Stammtisch 2020-01',
-            'Stammtisch 2019-12'
-        ];
-        $registrations = [];
-        foreach($eventIds as $eventId) {
-            $registrations[$eventId] = self::get_by_event_title($eventId);
-        }
-        return view('legacy.pastevents', compact('registrations'));
-    }*/
 
     public static function statistics() {
         $data = [];
@@ -84,30 +46,10 @@ class RegistrationLegacyController extends Controller
         return DB::select('SELECT participant_name, comment, virtual_flag, deleted_flag FROM registration_legacies WHERE id = ? AND deleted_flag = ? ORDER BY created_at DESC', [$id, 0]) ?? [];
     }
 
-/*    public static function getEventnameParticipantCountForAll() :array {
-        $events = self::getEventNames();
-        foreach($events as $event) {
-            $eventObject = new \stdClass();
-            $eventObject->event = substr($event->event, 11);
-            $count = self::getCountForEvent($event->event);
-            $eventObject->participants = self::getCountForEvent($event->event);
-            $eventArray[] = $eventObject;
-        }
-        return $eventArray;
-    }*/
-
     private static function getEventNames() :array {
         $sql = "SELECT DISTINCT event FROM registration_legacies";
         $sql .= " WHERE deleted_flag = ?";
         $sql .= " ORDER BY event ASC";
         return DB::select($sql, [0]) ?? [];
     }
-
-/*    private static function getCountForEvent(string $event) :int {
-        $sql = "SELECT COUNT(*) AS COUNT FROM registration_legacies";
-        $sql .= " WHERE deleted_flag = ?";
-        $sql .= " AND event = ?";
-        $result = DB::select($sql, [0, $event]) ?? 0;
-        return $result[0]->COUNT;
-    }*/
 }
