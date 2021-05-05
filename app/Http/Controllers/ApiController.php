cbe2af6ee19ac628ec6ad90e56c119d76329c6ef
Christoph Dunkake <christoph@dragoneyes.org>
2021-05-05 15:05:49 +0200

    get event fields selected

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -62,11 +62,11 @@ class ApiController extends Controller
     public static function get_by_id($id) {
         $log = \App\Http\Controllers\LogLegacyController::write('apirequest', "/event/" . $id);
         // not a good idea to send all fields!
-        $event = Event::find((int)$id);
+//        $event = Event::find((int)$id);
         // better be selective
-//        $event = Event::select('id', 'date', 'title', 'description', 'is_online', 'is_onsite', 'is_registration_open', 'updated_at')
-//            ->where('id', $id)
-//            ->get();
+        $event = Event::select('id', 'date', 'title', 'description', 'is_online', 'is_onsite', 'is_registration_open', 'updated_at')
+            ->where('id', $id)
+            ->get();
         if(!$event) {
             return [
                 "result" => "error",
